777d31bcfbd2d3f5872f27224f58d639f8305605
Kurniawan <kurnhyalcantara@gmail.com>
2023-05-18 11:46:18 +0800

    improve: skema menampilkan output dirombak total

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/fatih/color"
 	_ "github.com/go-sql-driver/mysql"
@@ -40,45 +41,31 @@ func main() {
 	}
 
 	defer db.Close()
-	for {
-		var choice int
-		userSession := controllers.CheckLoginSession(db)
-		if userSession == "" {
-			color.HiBlue("=========================================")
-			color.HiBlue("=           Account Service App         =")
-			color.HiBlue("=========================================")
-			fmt.Printf("\n")
-			fmt.Println("Menu:")
-			fmt.Println("--Account")
-			fmt.Printf("\t1. Sign Up\n\t2. Login\n")
-			fmt.Println("--Others")
-			fmt.Printf("\t10. Cari User\n\t0. Keluar")
-			fmt.Printf("\n")
-			fmt.Printf("Pilih menu: ")
-			fmt.Scanln(&choice)
-		} else {
-			color.HiBlue("=========================================")
-			color.HiBlue("=           Account Service App         =")
-			color.HiBlue("=========================================")
-			fmt.Printf("Anda login sebagai: ")
-			color.HiGreen(userSession)
-			fmt.Printf("\n")
-			fmt.Println("Menu:")
-			fmt.Println("--Account")
-			fmt.Printf("\t3. Profile\n\t4. Edit Account\n\t5. Delete Account\n")
-			fmt.Println("--Action")
-			fmt.Printf("\t6. Top Up\n\t7. Transfer\n\t8. History Top Up\n\t9. History Transfer\n")
-			fmt.Println("--Others")
-			fmt.Printf("\t10. Cari User\n\t0. Keluar")
-			fmt.Printf("\n")
-			fmt.Printf("Pilih menu: ")
-			fmt.Scanln(&choice)
-		}
+	
+	clearScreen()
+	var choice int = 99;
+	userSession := controllers.CheckLoginSession(db)
+	userData, err := controllers.GetLoggedInUser(db, userSession)
+	if err != nil {
+		fmt.Println(err.Error())
+		userData = &entities.Users{}
+	}
 
-		switch choice {
-		//Fitur Register
-		case 1:
+	for userSession == "" {
+		clearScreen()
+		fmt.Printf("\n")
+		fmt.Println("Menu:")
+		fmt.Println("--Account")
+		fmt.Printf("\t1. Sign Up\n\t2. Login\n")
+		fmt.Println("--Others")
+		fmt.Printf("\t10. Cari User\n\t0. Keluar")
+		fmt.Printf("\n")
+		fmt.Printf("Pilih menu: ")
+		fmt.Scanln(&choice)
+		for choice == 1 {
+			clearScreen()
 			var name, phone, password string
+			fmt.Printf("==== Buat Akun Baru ====\n")
 			fmt.Printf("Masukkan Nama Lengkap: ")
 			fmt.Scanln(&name)
 			fmt.Printf("Masukkan Kata Sandi: ")
@@ -91,17 +78,21 @@ func main() {
 				Phone:    phone,
 				Password: password,
 			}
-
 			newID, err := controllers.AddUser(db, user)
 			if err != nil {
 				fmt.Println("Gagal register: ", err.Error())
 			} else {
 				fmt.Println("Berhasil Register! ID Pengguna baru:", newID)
+				fmt.Println("Press Enter to continue")
+				fmt.Scanln()
+				choice = 99
+				break
 			}
-
-		case 2:
-
+		}
+		for choice == 2 {
+			clearScreen()
 			var phone, password string
+			fmt.Println("Login")
 			fmt.Printf("No. Hp: ")
 			fmt.Scanln(&phone)
 			fmt.Printf("Password: ")
@@ -111,66 +102,142 @@ func main() {
 			if err != nil {
 				fmt.Println(err)
 			} else {
+				clearScreen()
 				color.HiGreen("Login Berhasil")
 				fmt.Printf("Selamat datang %s!\nLogin at: %s\n", data[0], data[1])
+				fmt.Println("Press enter untuk masuk ke menu")
+				fmt.Scanln()
+				userSession = controllers.CheckLoginSession(db)
+				userDataLogin, err := controllers.GetLoggedInUser(db, userSession)
+				if err != nil {
+					fmt.Println(err.Error())
+					userData = &entities.Users{}
+				}
+				userData = userDataLogin
+				choice = 99
+				break
 			}
-
-		case 3:
-			// Pilihan menu 3
-			user, err := controllers.GetLoggedInUser(db, userSession)
-			if err != nil {
-				log.Println("Error:", err.Error())
-			} else {
-				controllers.ShowUser(user)
-			}
-
-		case 5:
-			var opt string
-			fmt.Printf("Apakah anda yakin ingin menghapus akun anda? (Y/N): ")
-			fmt.Scanln(&opt)
-			if opt == "Y" {
-				controllers.DeleteUser(db)
-			}
-
-		case 10:
+		}
+		for choice == 10 {
+			clearScreen()
 			var phone string
 			fmt.Printf("Masukkan Nomor Telpon User: ")
 			fmt.Scanln(&phone)
 			user := controllers.SearchUser(db, phone)
 			fmt.Printf("Nama: %s\n", user.Name)
 			fmt.Printf("No. Hp: %s\n", user.Phone)
-		case 7: 
-			var receiver, method string
-			var total uint64
-			fmt.Printf("Masukkan Nomor Telepon Penerima: ")
-			fmt.Scanln(&receiver)
-			fmt.Printf("Masukkan Metode Transfer: ")
-			fmt.Scanln(&method)
-			fmt.Printf("Masukkan jumlah yang ingin ditransfer: ")
-			fmt.Scanln(&total)
-			transferId := controllers.AddTransfer(db, receiver, method, total)
-			fmt.Printf("Sukses melakukan tranfer, Tranfer ID: %s", transferId)
-		
-		case 9:
-			transferHistory := controllers.GetHistoryTransfer(db, userSession)
-			for _, transfer := range transferHistory {
-				userReceiver, err := controllers.GetLoggedInUser(db, transfer.ReceiverId)
+		}
+	}
+
+	for userSession != "" && choice == 99 {
+		clearScreen()
+		fmt.Printf("Anda login sebagai: ")
+			color.HiGreen(userData.Name)
+			fmt.Printf("\n")
+			fmt.Println("Menu:")
+			fmt.Println("--Account")
+			fmt.Printf("\t3. Profile\n\t4. Edit Account\n\t5. Delete Account\n")
+			fmt.Println("--Action")
+			fmt.Printf("\t6. Top Up\n\t7. Transfer\n\t8. History Top Up\n\t9. History Transfer\n")
+			fmt.Println("--Others")
+			fmt.Printf("\t10. Cari User\n\t0. Keluar")
+			fmt.Printf("\n")
+			fmt.Printf("Pilih menu: ")
+			fmt.Scanln(&choice)
+			for choice == 3 {
+				user, err := controllers.GetLoggedInUser(db, userSession)
 				if err != nil {
-					log.Fatal(err.Error())
+					log.Println("Error:", err.Error())
+				} else {
+					clearScreen()
+					controllers.ShowUser(user)
+					fmt.Println("Press Enter untuk melanjutkan")
+					fmt.Scanln()
+					choice = 99
 				}
-				userSender, err := controllers.GetLoggedInUser(db, userSession)
-				if err != nil {
-					log.Fatal(err.Error())
+			}
+			for choice == 5 {
+				var opt string
+				fmt.Printf("Apakah anda yakin ingin menghapus akun anda? (Y/N): ")
+				fmt.Scanln(&opt)
+				if opt == "Y" {
+					controllers.DeleteUser(db)
+					userSession = controllers.CheckLoginSession(db)
+					break
 				}
-				fmt.Printf("\tID transfer: %s\n", transfer.TransferId)
-				fmt.Printf("\tNama Penerima: %s\n", userReceiver.Name)
-				fmt.Printf("\tNama Pengirim: %s\n", userSender.Name)
-				fmt.Printf("\tJumlah Transfer: %d\n", transfer.Total)
-				fmt.Printf("\tMethod Transfer: %s\n", transfer.MethodTransfer)
-				fmt.Printf("\tCreated At: %s\n", transfer.CreatedAt)
-				fmt.Println("\n========")
+			}
+			for choice == 7 {
+				clearScreen()
+				var receiver, method string
+				var total uint64
+				fmt.Printf("Masukkan Nomor Telepon Penerima: ")
+				fmt.Scanln(&receiver)
+				fmt.Printf("Masukkan Metode Transfer: ")
+				fmt.Scanln(&method)
+				fmt.Printf("Masukkan jumlah yang ingin ditransfer: ")
+				fmt.Scanln(&total)
+				transferId := controllers.AddTransfer(db, receiver, method, total)
+				fmt.Printf("Sukses melakukan tranfer, Tranfer ID: %s\n", transferId)
+				fmt.Println("Press Enter untuk kembali ke menu")
+				fmt.Scanln()
+				choice = 99
+			}
+			for choice == 9 {
+				clearScreen()
+				transferHistory := controllers.GetHistoryTransfer(db, userSession)
+				for _, transfer := range transferHistory {
+					userReceiver, err := controllers.GetLoggedInUser(db, transfer.ReceiverId)
+					if err != nil {
+						log.Fatal(err.Error())
+					}
+					userSender, err := controllers.GetLoggedInUser(db, userSession)
+					if err != nil {
+						log.Fatal(err.Error())
+					}
+					fmt.Println("\n========================================")
+					fmt.Printf("\tID transfer: %s\n", transfer.TransferId)
+					fmt.Printf("\tNama Penerima: %s\n", userReceiver.Name)
+					fmt.Printf("\tNama Pengirim: %s\n", userSender.Name)
+					fmt.Printf("\tJumlah Transfer: %d\n", transfer.Total)
+					fmt.Printf("\tMethod Transfer: %s\n", transfer.MethodTransfer)
+					fmt.Printf("\tWaktu Transfer: %s\n", transfer.CreatedAt)
+				}
+				fmt.Println("\nPress Enter untuk kembali ke menu")
+				fmt.Scanln()
+				choice = 99
+			}
+			for choice == 10 {
+				clearScreen()
+				var phone string
+				fmt.Printf("Masukkan Nomor Telpon User: ")
+				fmt.Scanln(&phone)
+				user := controllers.SearchUser(db, phone)
+				fmt.Printf("Nama: %s\n", user.Name)
+				fmt.Printf("No. Hp: %s\n", user.Phone)
+				fmt.Println("\nPress Enter untuk kembali ke menu")
+				fmt.Scanln()
+				choice = 99
 			}
 		}
-
+		if choice == 0 {
+			fmt.Println("Terima kasih telah bertransaksi...")
+			os.Exit(0)
+		}
 	}
-}
+
+	func displayBanner() {
+		color.HiBlue("=========================================")
+		color.HiBlue("=           Account Service App         =")
+		color.HiBlue("=========================================")
+	}
+
+	func clearScreen() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		displayBanner()
+	}
+
+
+
+
